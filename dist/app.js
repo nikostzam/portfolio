cef425bbf56504c0f2ab771359655220a6fc1a8c
nikostzam <nikosczt@gmail.com>
2020-02-02 02:28:20 +0200

    first commit

---
+++ dist/app.js
@@ -0,0 +1,7 @@
+particlesJS.load('particles-js', 'particles.json');
+
+var tl = new TimelineMax();
+tl.from("#line", 3.5, { scaleX: 0, transformOrigin: "right"});
+tl.from("#upper", 1.75, { opacity:0, y: 40 }, "text");
+tl.from("#lower", 1.75, { opacity:0, y: -40 }, "text");
+tl.to("#line, #upper, #lower", 1, { opacity: 1 });
