396ee00940d2b8aaa43a400184012b92d1a707ec
nikostzam <nikosczt@gmail.com>
2020-02-14 01:26:25 +0200

    Updates

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ tl.from("#line", 3.5, { scaleX: 0, transformOrigin: "right"});
 tl.from("#upper", 1.75, { opacity:0, y: 40 }, "text");
 tl.from("#lower", 1.75, { opacity:0, y: -40 }, "text");
 tl.to("#line, #upper, #lower", 1, { opacity: 1 });
+
+const icon = document.querySelector('.menu-img');
+const menu = document.querySelector('.nav-links');
+icon.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
